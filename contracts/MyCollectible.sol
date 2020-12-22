56bc2a4b2ce8cd7e55016ef001486e7250e47583
aadorian <aadorian@gmail.com>
2020-12-22 18:32:43 +0000

    Fist Commit

--- contracts/MyCollectible.sol
+++ contracts/MyCollectible.sol
@@ -1,9 +1,5 @@
-Sending gasless transactions
-
-based on https://docs.openzeppelin.com/learn/sending-gasless-transactions
-
-
-pragma solidity ^0.6.0;
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.4.22 <0.9.0;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
